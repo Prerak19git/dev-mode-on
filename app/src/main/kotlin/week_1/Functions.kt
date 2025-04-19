95c43e446522eb2684623a98f0faa883ef9a622d
Prerak <prerak.saxena1912@gmail.com>
2025-04-19 13:26:19 +0530

    feat : basic functions, enum class, vararg , named parameters, default values

---
+++ app/src/main/kotlin/week_1/Functions.kt
@@ -0,0 +1,44 @@
+package org.example.app.week_1
+
+
+enum class Operation {
+    SQUARE,
+    CUBE;
+}
+
+fun performOperation(number: Int, operation: Operation): Int {
+    return when (operation) {
+        Operation.SQUARE -> {
+            number * number
+        }
+
+        Operation.CUBE -> {
+            number * number * number
+        }
+    }
+}
+
+fun greet(personToGreet: String = "Prerak") {
+    println("Hi there, I am here to greet you!  Welcome $personToGreet")
+}
+
+fun printTheNumbers(vararg numbers: String) {
+    numbers.forEach {
+        print("$it ")
+    }
+    println()
+}
+
+fun oneLiner() = println("I am just a simple one liner function")
+
+
+fun main() {
+
+    greet()
+    greet("Drake")
+    println(performOperation(25, Operation.SQUARE))
+    println(performOperation(25, Operation.CUBE))
+    printTheNumbers("1", "2", "4", "5")
+    oneLiner()
+
+}
